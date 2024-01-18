c1eea4a72734556dc3f79a6a59e69ed072f2aef4
Yudi454 <lucasyudi445@gmail.com>
2024-01-18 14:07:21 -0300

    Implemente la creación de imagenes para el carrusel de Comidas y la tabla donde se muestra las distintas imagenes

--- src/pages/admin/Carrusel/CarruselComida/CrearImagenComida.jsx
+++ src/pages/admin/Carrusel/CarruselComida/CrearImagenComida.jsx
@@ -20,9 +20,159 @@ const CrearImagenComida = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  let posicionPrimera = ""
+  let posicionSegunda = ""
+  let posicionTercera = ""
+
+{if (imagenesComidas) {
+posicionPrimera = imagenesComidas.find((imagen) => imagen.Position === "Primera")
+posicionTercera = imagenesComidas.find((imagen) => imagen.Position === "Tercera")
+posicionSegunda = imagenesComidas.find((imagen) => imagen.Position === "Segunda")
+}}
+
+
+  const esquemaImagenPromocional = Yup.object().shape({
+    Imagen: Yup.string().required("El nombre es requerido"),
+
+    Posicion: Yup.string().required("La posición es requerida"),
+  });
+
+  const valoresIniciales = {
+    Imagen: "",
+    Posicion: "",
+  };
+
+  const formik = useFormik({
+    initialValues: valoresIniciales,
+    validationSchema: esquemaImagenPromocional,
+    validateOnChange: true,
+    validateOnBlur: true,
+    onSubmit: (values) => {
+
+      try {
+        Swal.fire({
+          title: "Estas seguro de crear esta imagen?",
+          text: "Luego lo puede modificar",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Si, estoy seguro!",
+          cancelButtonText: "No, mejor no",
+        }).then(async (result) => {
+          if (result.isConfirmed) {
+            const formData = new FormData();
+            formData.append("Image", values.Imagen);
+            formData.append("Position", values.Posicion);
+            formData.append("Function", "Comida");
+
+            const response = await axios.post(`${back}/ImgCarrusel`, formData, {
+              headers: {
+                "Content-Type": "multipart/form-data",
+              },
+            });
+
+            traerComidasCarrusel();
+            handleClose();
+            formik.resetForm();
+
+            Swal.fire(
+              "Imagen creada!",
+              "Creación realzada exitosamente",
+              "success"
+            );
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
   return (
     <>
-      <h1>c</h1>
+       <Button variant="primary" onClick={handleShow}>
+        Crear Imagen
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Formulario para Crear una Imagen en el Carrusel de Comidas</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={formik.handleSubmit} noValidate>
+          <Modal.Body>
+            <Stack gap={2}>
+              <Form.Group>
+                <Form.Label>Imagen</Form.Label>
+                <Form.Control
+                  type="file"
+                  name="Imagen"
+                  onChange={(e) => {
+                    formik.setFieldValue("Imagen", e.currentTarget.files[0]);
+                  }}
+                  className={clsx(
+                    "form-control",{
+                        "is-invalid" : formik.touched.Imagen && formik.errors.Imagen
+                    },{
+                        "is-valid" : formik.touched.Imagen && !formik.errors.Imagen
+                    }
+                  )}
+                />
+                {formik.touched.Imagen && formik.errors.Imagen && (
+                  <div>
+                    <span className="text-danger">{formik.errors.Imagen}</span>
+                  </div>
+                )}
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Posción:</Form.Label>
+                <Form.Select
+                  id="Posicion"
+                  {...formik.getFieldProps("Posicion")}
+                  className={clsx(
+                    "form-control",
+                    {
+                      "is-invalid":
+                        formik.touched.Posicion && formik.errors.Posicion,
+                    },
+                    {
+                      "is-valid":
+                        formik.touched.Posicion && !formik.errors.Posicion,
+                    }
+                  )}
+                >
+                  <option value="">Selecciona una opción</option>
+                  {!posicionPrimera && (
+                    <option value="Primera">Primera</option>
+                  )}
+                  {!posicionSegunda && (
+                    <option value="Segunda">Segunda</option>
+                  )}
+                  {!posicionTercera && (
+                    <option value="Tercera">Tercera</option>
+                  )}
+                </Form.Select>
+                {formik.touched.Posicion && formik.errors.Posicion && (
+                  <div>
+                    <span className="text-danger">
+                      {formik.errors.Posicion}
+                    </span>
+                  </div>
+                )}
+              </Form.Group>
+            </Stack>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit">
+              Crear Imagen
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+
     </>
   );
 };
--- src/pages/admin/Carrusel/CarruselComida/TablaImagenComida.jsx
+++ src/pages/admin/Carrusel/CarruselComida/TablaImagenComida.jsx
@@ -1,9 +1,160 @@
-import React from 'react'
+import { useContext, useEffect, useState } from "react";
+import { Button, Table, Modal } from "react-bootstrap";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { ProductosContext } from "../../../../context/Context";
+import EditarImagenPromocional from "../EditarImagenPromocional";
 
 const TablaImagenComida = () => {
+
+  const { PasarStates, imagenCarruselPorId, traerComidasCarrusel } =
+  useContext(ProductosContext);
+
+const { selectId, setSelectId, imagen, imagenesComidas } = PasarStates;
+
+const back = import.meta.env.VITE_API_BACK;
+
+const [show, setShow] = useState(false);
+const [currentPage, setCurrentPage] = useState(1);
+const [imagenesMostrar, setImagenesMostrar] = useState();
+const [imagenFiltrada, setImagenFiltrada] = useState("");
+const imagenesPerPage = 5;
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+
+{
+  imagenesComidas === undefined &&
+    (traerComidasCarrusel());
+}
+
+console.log(imagenesComidas);
+
+const indexOfLastImagen = currentPage * imagenesPerPage;
+const indexOfFirstImagen = indexOfLastImagen - imagenesPerPage;
+
+useEffect(() => {
+  if (imagenesComidas) {
+    const currentImagenes = imagenesComidas.slice(
+      indexOfFirstImagen,
+      indexOfLastImagen
+    );
+    setImagenesMostrar(currentImagenes);
+  }
+}, [imagenesComidas, indexOfFirstImagen, indexOfLastImagen]);
+
+const paginate = (pageNumber) => {
+  setCurrentPage(pageNumber);
+};
+
+useEffect(() => {
+  if (imagen == undefined && selectId) {
+    imagenCarruselPorId();
+  }
+}, [selectId]);
+
+
+
+const eliminarImagen = (id) => {
+  try {
+    Swal.fire({
+      title: "Estas seguro de eliminar la imagen?",
+      text: "No se podra recuperar",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, estoy seguro!",
+      cancelButtonText: "No, mejor no",
+    }).then( async (result) => {
+      if (result.isConfirmed) {
+        const res = await axios.delete(`${back}/ImgCarrusel/${id}`);
+        console.log(res);
+
+        traerComidasCarrusel();
+
+        Swal.fire(
+          "Imagen eliminada!",
+          "Eliminación realzada exitosamente",
+          "success"
+        );
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
   return (
     <>
-        <h1>a</h1>
+         <div className="Contenedor-Tabla-Chica">
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Imagen</th>
+              <th>Posicion</th>
+              <th>Funcion</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {!imagenesMostrar ? (
+              <tr>
+                <td>Cargando...</td>
+              </tr>
+            ) : imagenesMostrar.length <= 0 ? (
+              <tr>
+                <td colSpan="6" className="text-center">
+                  No se encontraron resultados.
+                </td>
+              </tr>
+            ) : (
+                imagenesMostrar.map((Imagen) => (
+                <tr key={Imagen._id}>
+                  <td>{Imagen._id}</td>
+                  <td>
+                    <img src={Imagen.Image} className="imagenTablas" alt="" />
+                  </td>
+                  <td>{Imagen.Position}</td>
+                  <td>{Imagen.Function}</td>
+                  <td>
+                    <Button
+                      onClick={(e) => {
+                        setSelectId(Imagen._id);
+                        handleShow();
+                      }}
+                    >
+                      Editar
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        eliminarImagen(Imagen._id);
+                        traerComidasCarrusel();
+                      }}
+                    >
+                      Borrar
+                    </Button>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </Table>
+        <EditarImagenPromocional show={show} setShow={setShow} handleClose={handleClose} />
+      </div>
+      <div className="pagination d-flex justify-content-center">
+        {imagenesComidas &&
+          imagenesComidas.length > imagenesPerPage &&
+          Array.from({
+            length: Math.ceil(imagenesComidas.length / imagenesPerPage),
+          }).map((_, index) => (
+            <button key={index} onClick={() => paginate(index + 1)}>
+              {index + 1}
+            </button>
+          ))}
+      </div>
     </>
   )
 }
